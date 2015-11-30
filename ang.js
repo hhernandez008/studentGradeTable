b47f3f69808bea6a744155df2086ff089fb88ac7
hhernandez008 <hhernandez008@gmail.com>
2015-11-30 10:53:12 -0800

    page loads information & works with angular objects

--- ang.js
+++ ang.js
@@ -3,6 +3,15 @@ var app = angular.module("sgtApp", []);
 app.controller("appController", function($scope){
     //Handles Student Loading from API
 
+    //adding students to dom
+    $scope.studentArray = [
+        //DUMMY DATA
+        {name: "Heather", course: "Math", grade: 98},
+        {name: "Andrew", course: "Math", grade: 100},
+        {name: "Nick", course: "Math", grade: 89},
+        {name: "Tanner", course: "Math", grade: 74}
+    ];
+
     this.calculateAverage = function(){
         var gradeQuantity = $scope.studentArray.length;
         var gradeSum = null;
@@ -11,34 +20,30 @@ app.controller("appController", function($scope){
         }
         //return grade average
         return Math.round(gradeSum/gradeQuantity);
-    }
+    };
 });
 
 app.controller("formController", function($scope){
     //Handles inputs & validation thru angular
     //add new student to array after successful ajax call, error handling
-    this.student = {};
+    this.newStudent = {};
 
     this.addStudent = function(){
         //verify valid inputs
-        this.student.grade = parseInt(this.student.grade);
-        $scope.studentArray.push(this.student);
-    }
+        //insert newStudent at the start of the array
+        $scope.studentArray.unshift(this.newStudent);
+        this.newStudent = {};
+    };
+
+    //auto-complete for name & course
 });
 
 app.controller("studentListController", function($scope){
-    //adding students to dom
-    $scope.studentArray = [
-        //DUMMY DATA
-        {name: "Heather", course: "Math", grade: 98},
-        {name: "David", course: "Math", grade: 100},
-        {name: "Chris", course: "Math", grade: 89},
-        {name: "Tanner", course: "Math", grade: 74}
-    ];
-
     //handle student delete & errors
     this.deleteStudent = function(num){
         //ajax call to delete student on success delete student
         $scope.studentArray.splice(num, 1);
-    }
+    };
+
+    //highlight min & max students
 });
