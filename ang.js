7503fdba3f4c3da604d7a723f7e284348fa67212
hhernandez008 <hhernandez008@gmail.com>
2015-12-03 14:47:05 -0800

    remane script file

--- ang.js
+++ ang.js
@@ -5,164 +5,189 @@ sgtApp.config(['$httpProvider', function ($httpProvider) {
     $httpProvider.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded";
 }]);
 
-sgtApp.factory("studentService", function($http){
-    var student = {};
-    student.studentArray = [];
+sgtApp.service("studentDataService", function ($http, $log) {
+    var student = this;
     var apiKey = "JhpapQQx34";
+    //Collect student data retrieved from database
+    student.studentArray = [];
 
-    student.dataObjToString = function(obj){
+    /**
+     * Take the passed in obj and convert it to a parameter string for use in url/param string creation.
+     * @param obj
+     * @returns {*}
+     */
+    student.dataObjToString = function (obj) {
         obj = $.param(obj);
         return obj;
     };
-    student.studentDataCall = function(){
-        return $http({
+
+    /**
+     * LOADING STUDENT DATA
+     */
+
+    student.loadError = false;
+    /**
+     * initial load and reload of student data
+     * @returns {*}
+     */
+    student.studentDataCall = function () {
+        $http({
             data: "api_key=" + apiKey,
             method: "post",
             url: "http://s-apis.learningfuze.com/sgt/get"
+        }).then(function (result) {
+            if (result.data.success) {
+                //save student data in reversed order
+                student.studentArray = result.data.data.reverse();
+            } else {
+                student.loadError = true;
+                $log.error(result.data.error[0]);
+            }
+        }, function () {
+            $log.error("Unsuccessful call to http://s-apis.learingfuze.com/sgt/get");
+            student.loadError = true;
         });
     };
-    student.studentAddCall = function(obj){
+    //return the student array
+    student.loadingResults = function () {
+        return student.studentArray;
+    };
+    //return a load error indication
+    student.loadingError = function () {
+        return student.loadError;
+    };
+
+    /**
+     * ADDING STUDENTS
+     */
+
+    student.addError = false;
+    /**
+     * Send the newly created student object to the database & on success add the student and
+     * the returned id to the student array
+     * @param obj
+     */
+    student.studentAddCall = function (obj) {
         var data = "api_key=" + apiKey + "&" + student.dataObjToString(obj);
-        console.log("studentAddCall data: ", data);
-        return $http({
+        $http({
             data: data,
             method: "post",
             url: "http://s-apis.learningfuze.com/sgt/create"
+        }).then(function (result) {
+            if (result.data.success) {
+                obj.id = result.data.new_id;
+                //add student to the start of the array
+                student.studentArray.unshift(obj);
+            } else {
+                student.addError = true;
+                $log.error(result.data.errors[0]);
+            }
+        }, function () {
+            $log.error("Unsuccessful call to http://s-apis.learningfuze.com/sgt/create");
+            student.addError = true;
         });
     };
-    return student;
-});
-
-sgtApp.controller("appController", function($scope, studentService){
-    var self = this;
-    self.loadError = false;
-    self.loading = false;
-
-    self.reloadStudents = function(){
-        self.loadError = false;
-        self.loadStudentsData();
+    //return add student error indicator
+    student.addingError = function () {
+        return student.addError;
     };
 
-    self.calculateAverage = function(){
-        var gradeQuantity = studentService.studentArray.length;
-        var gradeSum = null;
-        for(var i = 0; i < gradeQuantity; i++){
-            gradeSum += studentService.studentArray[i].grade;
-        }
-        //return grade average
-        return Math.round(gradeSum/gradeQuantity);
-    };
 
-    self.loadStudentsData = function(){
-        //show loading gif
-        self.loading = true;
-        studentService.studentDataCall()
-            .then(function(result){
-                //remove loading gif
-                self.loading = false;
-                var dataArr = result.data.data;
-                if(result.data.success){
-                    //empty array before loading data
-                    studentService.studentArray = [];
-                    //add student data from server
-                    for(var i = 0; i < dataArr.length; i++){
-                        //remove any excess whitespace before storing & adding to DOM
-                        dataArr[i].name = dataArr[i].name.trim();
-                        dataArr[i].course = dataArr[i].course.trim();
-                        dataArr[i].deleteError = false;
-                        //add student to array; object properties: id(num), name(string), grade(num), course(string)
-                        studentService.studentArray.unshift(dataArr[i]);
-                    }
-                }else {
-                    //show error message
-                    self.loadError = true;
-                }
-                console.log(studentService.studentArray);
-                //run apply of $scope service
-                //$scope.$apply();
-            },
-            function(response){
-                self.loading = false;
-                console.error("error");
-            }
-        );
-    }; //end loadStudentsData
+    /**
+     * DELETING STUDENTS
+     */
 
-    //load student data on controller load
-    self.loadStudentsData();
-}); //end appController
-
-sgtApp.controller("formController", function($scope, studentService){
-    //Handles inputs & validation thru angular
-    //add new student to array after successful ajax call, error handling
-    var self = this;
-    self.newStudent = {};
-    self.addStudentError = false;
-
-    self.addStudent = function(){
-        self.addStudentError = false;
-        //verify valid inputs
-        //insert newStudent at the start of the array
-        self.addStudentAjaxCall();
-    };
-
-    self.addStudentAjaxCall = function() {
-        studentService.studentAddCall(self.newStudent)
-            .then(function (response) {
-                if (response.success) {
-                    console.log("Student Added: ", self.newStudent);
-                    self.newStudent.id = response.new_id;
-                    self.newStudent.deleteError = false;
-                    //place object in studentArray
-                    studentService.studentArray.unshift(self.newStudent);
-
-                    //clear input form
-                    self.newStudent = {};
-                } else {
-                    self.addStudentError = true;
-                }
-                // $scope.$digest() cleared the newStudent object, but did not call the ng-repeat on the studentArray
-                $scope.$apply();
-            }, //end success function
-            function () {
-                self.addStudentError = true;
-                $scope.$apply();
-            }
-        );
-
-    };
-});
-
-sgtApp.controller("studentListController", function($scope, studentService){
-
-    this.deleteStudent = function(index){
-        //ajax call to delete student on success delete student
-        var studentID = studentService.studentArray[index].id;
-        this.deleteStudentAjaxCall(studentID, index);
-    };
-
-    this.deleteStudentAjaxCall = function(id, index){
-        $.ajax({
-            dataType: "json",
-            data: {api_key: $scope.key, student_id: id},
+    student.delteError = false;
+    student.deleteErrorMessage = "";
+    /**
+     * Send the id of the student to delete to the database.
+     * On success delete the student form the student array.
+     * @param studentId
+     * @param index
+     */
+    student.studentDeleteCall = function (studentId, index) {
+        var data = "api_key=" + apiKey + "&student_id=" + studentId;
+        $http({
+            data: data,
             method: "post",
-            url: "http://s-apis.learningfuze.com/sgt/delete",
-            success: function(result) {
-                if(result.success) {
-                    studentService.studentArray.splice(index, 1);
-                }else{
-                    studentService.studentArray[index].deleteError = true;
-                    studentService.studentArray[index].deleteErrorMessage = "You are not authorized to delete this student.";
-                }
-                $scope.$apply();
-            }, //end success function
-            error: function(){
-                studentService.studentArray[index].deleteError = true;
-                studentService.studentArray[index].deleteErrorMessage = "You are not authorized to delete this student.";
-                $scope.$apply();
+            url: "http://s-apis.learningfuze.com/sgt/delete"
+        }).then(function (result) {
+            if (result.data.success) {
+                student.studentArray.splice(index, 1);
+            } else {
+                student.deleteError = true;
+                student.deleteErrorMessage = "You are not authorized to delete this student";
+                $log.error(result.data.errors[0]);
             }
-        }); //end ajax call
+        }, function () {
+            student.deleteError = false;
+            student.deleteErrorMessage = "Unable to delete student at this time. Please try again.";
+            $log.error("Unsuccessful call to http://s-apis.learninfuze.com/sgt/create");
+        });
+    };
+    //return delete student error indicators
+    student.deletingError = function () {
+        return student.deleteError;
+    };
+    //return delete student error message
+    student.deletingErrorMessage = function(){
+        return student.deleteErrorMessage;
     };
 
-    //highlight min & max students
+    /**
+     * Reset all errors to original value
+     */
+    student.resetErrors = function () {
+        student.loadError = false;
+        student.addError = false;
+        student.deleteError = false;
+        student.deleteErrorMessage = "";
+    };
+}); //end studentDataService
+
+
+sgtApp.controller("appController", function (studentDataService) {
+    //make initial call to retrieve student data
+    studentDataService.studentDataCall();
+    //reload button clicked
+    this.reloadStudents = function () {
+        studentDataService.resetErrors();
+        studentDataService.studentDataCall();
+    };
+
+    //calculate average
+    this.calculateAverage = function () {
+        var gradeSum = 0;
+        var numGrades = 0;
+        for (var i = 0; i < studentDataService.studentArray.length; i++) {
+            gradeSum += studentDataService.studentArray[i].grade;
+            numGrades++;
+        }
+        return Math.round(gradeSum / numGrades);
+    };
+}).controller("formController", function (studentDataService) {
+    this.newStudent = {};
+    this.addStudentError = studentDataService.addingError;
+    //add student to database & update studentDataService.studentArray
+    this.addStudent = function () {
+        studentDataService.resetErrors();
+        studentDataService.studentAddCall(this.newStudent);
+        this.newStudent = {};
+    };
+    this.resetForm = function () {
+        studentDataService.resetErrors();
+        this.newStudent = {};
+    }
+}).controller("studentList", function (studentDataService) {
+    this.loadError = studentDataService.loadingError;
+    //copy of the studentDataService.studentDataCall
+    this.studentData = studentDataService.loadingResults;
+
+    this.deleteError = studentDataService.deletingError;
+    this.errorMessage = studentDataService.deletingErrorMessage;
+    this.deleteStudent = function (num) {
+        studentDataService.resetErrors();
+        var studentID = studentDataService.studentArray[num].id;
+        studentDataService.studentDeleteCall(studentID, num);
+    };
 });
